49ca9ec7ed15d775a7330ed2b75bc40b5eb680ee
Jack Lin <linjiajianyuan@gmail.com>
2016-01-18 22:48:13 -0800

    add try catch on main

--- EbayInventoryCon/Program.cs
+++ EbayInventoryCon/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PebbledonUtilityLib;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Text;
@@ -11,7 +13,23 @@ namespace EbayInventoryCon
     {
         static void Main(string[] args)
         {
-            EbayInventoryMdl.Mdl.UpdateEbayInventory();
+            string senderEmail = ConfigurationManager.AppSettings["senderEmail"];
+            string messageFromPassword = ConfigurationManager.AppSettings["messageFromPassword"];
+            string messageToEmail = ConfigurationManager.AppSettings["messageToEmail"];
+            string smtpClient = ConfigurationManager.AppSettings["smtpClient"];
+            int smtpPortNum = ConvertUtility.ToInt(ConfigurationManager.AppSettings["smtpPortNum"]);
+            try
+            {
+                EbayInventoryMdl.Mdl.UpdateEbayInventory();
+            }
+            catch (Exception ex)
+            {
+                ExceptionUtility exceptionUtility = new ExceptionUtility();
+                exceptionUtility.CatchMethod(ex, "UpdateEbayInventoryCon Main ", ex.Message.ToString(), senderEmail, messageFromPassword, messageToEmail, smtpClient, smtpPortNum);
+            }
+
+
+            
         }
     }
 }
