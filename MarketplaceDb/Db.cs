374d8c7d72eae86b304f614a9652e76232d16a90
Jack Lin <linjiajianyuan@gmail.com>
2016-01-24 22:37:36 -0800

    adding order deail form

--- MarketplaceDb/Db.cs
+++ MarketplaceDb/Db.cs
@@ -18,15 +18,15 @@ namespace MarketplaceDb
             string sql = "";
             if (shipped==0)
             {
-                sql = "select OrderNum,AccountName,OrderDate,BuyerUserID,Note,ShipName,ShipAddress1,ShipCity,ShipState,ShipZip,ShipPhone,ShippedDate,EnterDate,Channel from OrderHeader where ShippedDate='1753-01-01' and OrderDate >='" + startDate + "' and OrderDate <='" + endDate + "'";
+                sql = "select OrderNum,AccountName,OrderDate,BuyerUserID,Note,ShipName,ShipAddress1,ShipAddress2,ShipCity,ShipState,ShipZip,ShipPhone,ShippedDate,EnterDate,Channel from OrderHeader where ShippedDate='1753-01-01' and OrderDate >='" + startDate + "' and OrderDate <='" + endDate + "' order by OrderDate desc";
             }
             else if (shipped ==1)
             {
-                sql = "select OrderNum,AccountName,OrderDate,BuyerUserID,Note,ShipName,ShipAddress1,ShipCity,ShipState,ShipZip,ShipPhone,Note,ShippedDate,EnterDate,Channel from OrderHeader where ShippedDate <>'1753-01-01' and OrderDate >='" + startDate + "' and OrderDate <='" + endDate + "'";
+                sql = "select OrderNum,AccountName,OrderDate,BuyerUserID,Note,ShipName,ShipAddress1,ShipAddress2,ShipCity,ShipState,ShipZip,ShipPhone,ShippedDate,EnterDate,Channel from OrderHeader where ShippedDate <>'1753-01-01' and OrderDate >='" + startDate + "' and OrderDate <='" + endDate + "' order by OrderDate desc";
             }
             else
             {
-                sql = "select OrderNum,AccountName,OrderDate,BuyerUserID,Note,ShipName,ShipAddress1,ShipCity,ShipState,ShipZip,ShipPhone,Note,ShippedDate,EnterDate,Channel from OrderHeader where OrderDate >='" + startDate + "' and OrderDate <='" + endDate + "'";
+                sql = "select OrderNum,AccountName,OrderDate,BuyerUserID,Note,ShipName,ShipAddress1,ShipAddress2,ShipCity,ShipState,ShipZip,ShipPhone,ShippedDate,EnterDate,Channel from OrderHeader where OrderDate >='" + startDate + "' and OrderDate <='" + endDate + "' order by OrderDate desc";
             }
             try
             {
---
+++ MarketplaceWinForm/OrderDetailForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarketplaceWinForm
+{
+    public partial class OrderDetailForm : Form
+    {
+        public OrderDetailForm(string orderId)
+        {
+            InitializeComponent();
+            this._OrderNumTxt.ReadOnly = true;
+            this._OrderNumTxt.Text = orderId;
+            
+        }
+
+        private void OrderDetailForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
